6891ded115464c5a34f155eed593cc9fe4fb5f19
Pulimood Team 1 Development <team1d@csprojects.tcnj.edu>
2013-11-12 10:33:15 -0500

    Removing the drop table .sql file.

--- brown2green/dropDABEAT.sql
+++
@@ -1,7 +0,0 @@
-psql -t -d team1d -c "SELECT 'DROP TABLE ' || n.nspname || '.' ||
-c.relname || ' CASCADE;' FROM pg_catalog.pg_class AS c LEFT JOIN
-pg_catalog.pg_namespace AS n ON n.oid = c.relnamespace WHERE relkind =
-'r' AND n.nspname NOT IN ('pg_catalog', 'pg_toast') AND
-pg_catalog.pg_table_is_visible(c.oid)" >/tmp/droptables
-
-psql -d team1d -f /tmp/droptables
